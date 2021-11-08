eea7e59a4b16632cb3834c20d5393d8a71d58f68
ZephyrR33 <kasorin767@gmail.com>
2021-11-08 11:31:06 +0300

    New Update is now

---
+++ app.js
@@ -0,0 +1,19 @@
+function changeColorRed(){
+    //document.body.style.background = colorArray[i];
+    document.body.style.backgroundImage = "url('/FonRed.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+}
+
+function changeColorPink(){
+    //document.body.style.background = colorArray[i];
+    document.body.style.backgroundImage = "url('/FonPink.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+}
+
+function changeColorGreen(){
+    //document.body.style.background = colorArray[i];
+    document.body.style.backgroundImage = "url('/FonGreen.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+}
+
+function changeColorBlue(){
+    //document.body.style.background = colorArray[i];
+    document.body.style.backgroundImage = "url('/FonBlue.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+}
