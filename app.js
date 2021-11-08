0ae4e66cce25662c8eea76e4a986b1faae4210ff
ZephyrR33 <kasorin767@gmail.com>
2021-11-08 12:21:14 +0300

    FUpdate

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 function changeColorRed(){
     //document.body.style.background = colorArray[i];
-    document.body.style.backgroundImage = "url('/FonRed.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonRed.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
 }
 
 function changeColorPink(){
     //document.body.style.background = colorArray[i];
-    document.body.style.backgroundImage = "url('/FonPink.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonPink.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
 }
 
 function changeColorGreen(){
     //document.body.style.background = colorArray[i];
-    document.body.style.backgroundImage = "url('/FonGreen.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonGreen.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
 }
 
 function changeColorBlue(){
     //document.body.style.background = colorArray[i];
-    document.body.style.backgroundImage = "url('/FonBlue.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonBlue.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
 }
