c3a77e961b804d543e1c5413dd052b38a3c2cee9
ZephyrR33 <kasorin767@gmail.com>
2021-11-11 02:37:25 +0300

    Many reform

--- app.js
+++ app.js
@@ -1,19 +1,65 @@
-function changeColorRed(){
+function changeColorRed() {
     //document.body.style.background = colorArray[i];
-    document.body.style.background = "url('/FonRed.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonRed.png') center no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.margin = "0";
+    document.body.style.backgroundColor = "#f46553";
+    document.body.style.padding = "0";
+    document.body.style.backgroundPosition = "top";
+    document.body.style.backgroundSize = "auto";
+    document.body.style.backgroundAttachment = "fixed";
 }
 
-function changeColorPink(){
+
+function changeColorPink() {
     //document.body.style.background = colorArray[i];
-    document.body.style.background = "url('/FonPink.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonPink.png') center  no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+
+
+
+
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+
+    document.body.style.backgroundPosition = "top";
+
+    document.body.style.backgroundSize = "auto";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundColor = "#fc90ed";
+
+
+
 }
 
-function changeColorGreen(){
+function changeColorGreen() {
     //document.body.style.background = colorArray[i];
-    document.body.style.background = "url('/FonGreen.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonGreen.png') center no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+
+    document.body.style.backgroundPosition = "top";
+
+    document.body.style.backgroundSize = "auto";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundColor = "#66cc99";
+
+
 }
 
-function changeColorBlue(){
+function changeColorBlue() {
     //document.body.style.background = colorArray[i];
-    document.body.style.background = "url('/FonBlue.png')"; //Если файл в корне, если путь другой, укажите путь перед t.png
+    document.body.style.background = "url('/FonBlue.png') no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+
+
+
+    document.body.style.margin = "0";
+    document.body.style.padding = "0";
+
+    document.body.style.backgroundPosition = "top";
+
+    document.body.style.backgroundSize = "auto";
+    document.body.style.backgroundAttachment = "fixed";
+    document.body.style.backgroundColor = "#003366";
+
+
+
 }
