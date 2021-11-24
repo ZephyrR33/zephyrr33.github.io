839dfd6b80c4f7b87035dd31b85da0668e32071b
ZephyrR33 <kasorin767@gmail.com>
2021-11-24 19:44:58 +0300

    HggH

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function changeColor() {
     var a = parseInt(localStorage.getItem("anumber"));
     if (a == 1) {
         //document.body.style.background = colorArray[i];
-        document.body.style.background = "url('/FonPink.png') center  no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+        document.body.style.background = "url('/FonDark.png') center  no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
 
 
 
@@ -39,11 +39,11 @@ function changeColor() {
 
         document.body.style.backgroundSize = "auto";
         document.body.style.backgroundAttachment = "fixed";
-        document.body.style.backgroundColor = "#fc90ed";
+        document.body.style.backgroundColor = "#000000";
     }
 
     if (a == 2) {
-        document.body.style.background = "url('/FonBlue.png') no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+        document.body.style.background = "url('/FonObloko.png') no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
 
 
 
@@ -54,11 +54,11 @@ function changeColor() {
 
         document.body.style.backgroundSize = "auto";
         document.body.style.backgroundAttachment = "fixed";
-        document.body.style.backgroundColor = "#003366";
+        document.body.style.backgroundColor = "#330000";
     }
 
     if (a == 3) {
-        document.body.style.background = "url('/FonGreen.png') center no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+        document.body.style.background = "url('/FonVan.png') center no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
         document.body.style.margin = "0";
         document.body.style.padding = "0";
 
@@ -66,13 +66,13 @@ function changeColor() {
 
         document.body.style.backgroundSize = "auto";
         document.body.style.backgroundAttachment = "fixed";
-        document.body.style.backgroundColor = "#66cc99";
+        document.body.style.backgroundColor = "#003300";
     }
 
     if (a == 4) {
-        document.body.style.background = "url('/FonRed.png') center no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
+        document.body.style.background = "url('/FonSaske.png') center no-repeat"; //Если файл в корне, если путь другой, укажите путь перед t.png
         document.body.style.margin = "0";
-        document.body.style.backgroundColor = "#f46553";
+        document.body.style.backgroundColor = "#000033";
         document.body.style.padding = "0";
         document.body.style.backgroundPosition = "top";
         document.body.style.backgroundSize = "auto";
