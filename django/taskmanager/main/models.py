f8cd1ca10b501094d46e22d544f64d9c3d9ce609
eureu <venekbespalov@mail.ru>
2022-04-08 00:43:14 +0300

    '

--- django/taskmanager/main/models.py
+++ django/taskmanager/main/models.py
@@ -1,4 +1,5 @@
 from statistics import mode
+from tabnanny import verbose
 from django.db import models
 
 # Create your models here.
@@ -7,9 +8,22 @@ class Task(models.Model):
     task = models.TextField('Описание')
 
 
-def __str__(self):
-    return self.title
+
+class onepunchman(models.Model):
+    title = models.CharField('Название', max_length=250)
+    sezon = models.CharField('Сезон', max_length=250)
+    episode = models.CharField('Эпизод', max_length=250)
+    res1080 = models.CharField('res1080', max_length=250)
+    res720 = models.CharField('res720', max_length=250)
+    res480 = models.CharField('res480', max_length=250)
+    res360 = models.CharField('res360', max_length=250)
 
 
-def vanpunchman(self):
-    pass
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'onepunchman'            #переименование (единственное число)
+        verbose_name_plural = 'onepunchmen'     #переименование (множественное число)
+
