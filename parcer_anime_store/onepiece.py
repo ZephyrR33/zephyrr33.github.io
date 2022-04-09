bb5d3cd9a2086f0db9309326da36604675150c9b
ZephyrR33 <kasorin767@gmail.com>
2022-04-09 15:21:25 +0300

    rfgdfdfh

--- parcer_anime_store/onepiece.py
+++ parcer_anime_store/onepiece.py
@@ -1,12 +1,12 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
-HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36', 
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36', 
     'accept': '*/*'
 }
 #Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36
 #Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36
-#Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36
+
 page = 1
 counter = 1
 series = []
@@ -16,8 +16,9 @@ season = 1
 # теперь это ванпанчмен
 
 
-while counter < 2:
-    URL = f'https://jut.su/one-punchman/season-{season}/episode-{page}.html'
+while counter < 13:
+    URL = f'https://jut.su/one-punchman/season-1/episode-{page}.html'
+    
     r = requests.get(URL, headers=HEADERS, params = None)
     soup = bs(r.content, 'html.parser')
     items = soup.findAll('div', class_='videoContent')
