46eb60e3e09d0cc8cf3903984763e7660df4c4dd
eureu <venekbespalov@mail.ru>
2022-04-11 15:01:54 +0300

    '

--- django/taskmanager/main/views.py
+++ django/taskmanager/main/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 from bs4 import BeautifulSoup as bs
-
+from fake_useragent import UserAgent
 
 # Create your views here.
 def index(request):
@@ -71,7 +71,10 @@ def opm1(request):
 
 # парсер с ссылками на аниме (конкретно здесь сейчас ванпанчмен)
 def url_punch_season1_ep_1():
-    HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36', 
+
+    ua = UserAgent()
+
+    HEADERS = {'user-agent': 'ua["google chrome"]', 
     'accept': '*/*'
     }
     page = 1
