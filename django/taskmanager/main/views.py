7f948623a68fc7c946b0d12d2769a4b29e579c6f
eureu <venekbespalov@mail.ru>
2022-04-11 04:22:12 +0300

    '

--- django/taskmanager/main/views.py
+++ django/taskmanager/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
+from bs4 import BeautifulSoup as bs
 
 
 # Create your views here.
@@ -64,5 +66,42 @@ def mb1(request):
 
 
 def opm1(request):
-    return render(request, 'main/onepunchman/episode-1.html')
+    return render(request, 'main/onepunchman/episode-1.html', {'url': url_punch_season1_ep_1()[0]['episode1']['res1080']})
+
+
+# парсер с ссылками на аниме (конкретно здесь сейчас ванпанчмен)
+def url_punch_season1_ep_1():
+    HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36', 
+    'accept': '*/*'
+    }
+    page = 1
+    counter = 1
+    series = []
+    urls = []
+    season = 1
+    while counter < 13:
+        URL = f'https://jut.su/one-punchman/season-1/episode-{page}.html'
+    
+        r = requests.get(URL, headers=HEADERS, params = None)
+        soup = bs(r.content, 'html.parser')
+        items = soup.findAll('div', class_='videoContent')
+        for item in items:
+            for link in item.find_all('source'):
+                link1 = link.get('src')
+                urls.append(link1)
+            
+        series.append({
+            f'episode{page}': {'res1080': urls[0],
+            'res720': urls[1],
+            'res480': urls[2],
+            'res360': urls[3]
+                }
+        })
+        
+        urls.clear()
+        counter += 1
+        page += 1
+    return series
+    
+
 
