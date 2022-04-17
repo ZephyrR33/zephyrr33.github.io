65814ea458d8f7cc8414cd3eb1f93d800d279a15
eureu <venekbespalov@mail.ru>
2022-04-17 15:45:53 +0300

    +

--- parcer_anime_store/onepiece1.py
+++ parcer_anime_store/onepiece1.py
@@ -1,14 +1,35 @@
 from selenium import webdriver
 import time
+from selenium.webdriver.common.by import By
 
-browser = webdriver.Chrome(executable_path='D:\\programs_python\\parcer_anime_store\\chromedriver\\chromedriver.exe')
-browser.get('https://jut.su/one-piece/episode-1.html')
-search_from = browser.find_element_by_tag_name('html')
+browser = webdriver.Chrome(executable_path='D:\\programs_python\\zephyrr33.github.io\\parcer_anime_store\\chromedriver\\chromedriver')
 
-links = browser.find_element_by_tag_name('div')
-for i in links:
-    try:
-        video_link = i.get('src')
-        print(video_link)
-    except Exception:
-        pass
+#search_from = browser.find_element_by_tag_name('html')
+
+#browser = webdriver.Chrome()
+
+page = 1
+counter = 1
+series = []
+urls = []
+season = 1
+URL = f'https://jut.su/one-punchman/season-1/episode-{page}.html'
+browser.get(URL)   
+    
+#links = browser.find_element_by_tag_name('div', id='ghghjjj')
+
+#ur = browser.find_elements_by_tag_name('div').text
+'''ur = browser.find_elements(By.TAG_NAME, "div")
+time.sleep(10)
+
+for i in ur:
+    ur1 = i.find_element(By.CSS_SELECTOR, 'ghghjjj')
+    print(ur1.text)'''
+
+ur = browser.find_element(By.CLASS_NAME, value='vjs-tech')
+#for i in ur:
+ #   ur1 = i.find_element(By.TAG_NAME, value='script')
+
+print(ur.text)
+time.sleep(7)
+browser.quit()
