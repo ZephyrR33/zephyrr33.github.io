5f6334ebc76858c3f46732187c5a24befb7c590b
eureu <venekbespalov@mail.ru>
2022-04-28 18:36:01 +0300

    ;

--- django/taskmanager/main/admin.py
+++ django/taskmanager/main/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import onepunchman
+from .models import Genre, Anime, Rating, Ratingstar
 # Register your models here.
 
-admin.site.register(onepunchman)
+
+admin.site.register(Genre)
+admin.site.register(Anime)
+admin.site.register(Rating)
+admin.site.register(Ratingstar)
+
--- django/taskmanager/main/models.py
+++ django/taskmanager/main/models.py
@@ -27,3 +27,68 @@ class onepunchman(models.Model):
         verbose_name = 'onepunchman'            #переименование (единственное число)
         verbose_name_plural = 'onepunchmen'     #переименование (множественное число)
 
+
+
+class Genre(models.Model):
+    name = models.CharField('Название', max_length=250)
+    url = models.SlugField(max_length=260, unique=True)
+
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'Категория'            #переименование (единственное число)
+        verbose_name_plural = 'Категории'     #переименование (множественное число)
+
+
+
+class Anime(models.Model):
+    title = models.CharField('Название', max_length=250)
+    description = models.TextField('Описание', default='')
+    #poster = models.ImageField('Постер', upload_to='')
+    genre = models.ManyToManyField(Genre, verbose_name='Жанр')
+    pubdate = models.DateField('Дата выхода')
+    url = models.SlugField(max_length=260, unique=True)
+
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'Аниме'            #переименование (единственное число)
+        verbose_name_plural = 'Аниме'     #переименование (множественное число)
+
+
+
+class Ratingstar(models.Model):
+    value = models.SmallIntegerField('Значение', default=0)
+    
+
+    def __str__(self):
+        return self.value
+
+
+    class Meta:
+        verbose_name = 'Звезда рейтинга'            #переименование (единственное число)
+        verbose_name_plural = 'Звезды рейтинга'     #переименование (множественное число)
+
+
+
+class Rating(models.Model):
+    ip = models.CharField('IP adress', max_length=20)
+    star = models.ForeignKey(Ratingstar, on_delete=models.CASCADE, verbose_name='Звезда')
+    anime = models.ForeignKey(Anime, on_delete=models.CASCADE, verbose_name='Аниме')
+    
+
+    def __str__(self):
+        return f'{self.star} - {self.anime}'
+
+
+    class Meta:
+        verbose_name = 'Рейтинг'            #переименование (единственное число)
+        verbose_name_plural = 'Рейтинги'     #переименование (множественное число)
+
+
